8562e861c1cdc06921a10bef11c999708b9e86c1
Burn <76781080+burn-e-e@users.noreply.github.com>
2023-01-22 13:10:56 +0800

    Update simpleOOP1.java

---
+++ java_note/simpleOOP1.java
@@ -0,0 +1,10 @@
+public class Main {
+  int x = 5;
+
+  public static void main(String[] args) {
+    Main myObj1 = new Main();  // Object 1
+    Main myObj2 = new Main();  // Object 2
+    System.out.println(myObj1.x);
+    System.out.println(myObj2.x);
+  }
+}
