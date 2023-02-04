f8ede865143b5616150af1801f14a13614889e51
Burn <4burnee213@gmail.com>
2023-02-04 15:06:58 +0800

    Commit

--- c#_note/C# Console App/ConsoleApplication/ConsoleApplication/Program.cs
+++ c#_note/C# Console App/ConsoleApplication/ConsoleApplication/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int a, b, c;
+Console.WriteLine("Enter a number: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter a number: ");
+b = Convert.ToInt32(Console.ReadLine());
+c = a + b;
+Console.WriteLine("Answer is: " + c);
+
