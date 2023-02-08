18c12f750768752e7022dfdd620dc577b5093ab3
Burn <4burnee213@gmail.com>
2023-02-08 10:18:08 +0800

    Update OOP_2.java

--- java_note/OOP_2.java
+++ java_note/OOP_2.java
@@ -1,14 +1,10 @@
 // Create a Main class
 public class OOP_2 {
-  int x;  // Create a class attribute
-
-  // Create a class constructor for the Main class
-  public void Myclass() {
-    x = 5;  // Set the initial value for the class attribute x
-  }
-
+  int x = 5;  
   public static void main(String[] args) {
-    Myclass myObj = new Myclass(); // Create an object of class Main (This will call the constructor)
-    System.out.println(myObj.x); // Print the value of x
+    int y=10;
+    OOP_2 myObj = new OOP_2(); 
+    System.out.println(myObj.x); 
+    System.out.println(y); 
   }
 }
