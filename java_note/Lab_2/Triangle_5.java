476372b8b883ca57620c4696f23ae5abad62e6b8
Burn <4burnee213@gmail.com>
2023-02-09 09:20:49 +0800

    Update Triangle_5.java

--- java_note/Lab_2/Triangle_5.java
+++ java_note/Lab_2/Triangle_5.java
@@ -1,5 +1,17 @@
 public class Triangle_5 {
      public static void main(String[] args) {
-          
+          int[][] a = {
+                    { 0 },
+                    { 2, 4 },
+                    { 6, 8, 10 },
+                    { 12, 14, 16, 18 },
+                    { 20, 22, 24, 26, 28 },
+          };
+          for (int i = 0; i < a.length; i++) {
+               for (int j = 0; j < a[i].length; j++) {
+                    System.out.print(a[i][j] + " ");
+               }
+               System.out.println();
+          }
      }
 }
