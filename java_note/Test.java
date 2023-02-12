96ee18473bded78dd6e92044cc9837c7d453caee
Burn <76781080+burn-e-e@users.noreply.github.com>
2023-02-12 18:57:15 +0800

    Update Test.java

--- java_note/Test.java
+++ java_note/Test.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class Test
 {
      public static void main(String[] args){
-           System.out.println("Hello World");    
+          int a=2;
+           System.out.println("Hello World"+a);    
      }
 }
