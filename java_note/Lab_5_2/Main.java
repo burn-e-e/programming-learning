097b1deb3bb9656da258e9599f36239d368f280c
Burn <76781080+burn-e-e@users.noreply.github.com>
2023-03-01 12:36:03 +0800

    Private Public
    Private Public classes and functions trough packages.

---
+++ java_note/Lab_5_2/Main.java
@@ -0,0 +1,11 @@
+package Lab_5_2;
+
+import Lab_5_Box.Student;
+
+public class Main {
+     public static void main(String[] args) {
+          Student s1 = new Student();
+          s1.setName("Burnee");
+          System.out.println(s1.getName());
+     }
+}
---
+++ java_note/Lab_5_Box/Student.java
@@ -0,0 +1,11 @@
+package Lab_5_Box;
+
+public class Student {
+     private String name;
+     public void setName(String n){
+          name = n;
+     }
+     public String getName(){
+          return name;
+     }
+}
