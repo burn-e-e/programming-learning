1d84ff411e2de6f00e44db24236658cdd7b6169c
Burn <76781080+burn-e-e@users.noreply.github.com>
2023-03-06 11:27:11 +0800

    Edit

---
+++ java_note/OOP/Fibonacci.java
@@ -0,0 +1,15 @@
+package OOP;
+
+public class Fibonacci {
+     public static long fib(long n) {
+          if (n <= 1) {
+               return n;
+          } else {
+               return fib(n - 1) + fib(n - 2);
+          }
+     }
+
+     public static void main(String[] args) {
+          System.out.println(fib(3));
+     }
+}
