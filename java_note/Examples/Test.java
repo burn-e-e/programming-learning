221d9e92fffc13983c5e0a83d0ded259c6baf8a9
Burn <76781080+burn-e-e@users.noreply.github.com>
2023-03-15 11:35:10 +0800

    Del
    Deleted errors

--- java_note/Examples/Test.java
+++
@@ -1,9 +0,0 @@
-package Examples;
-import java.util.*;
-public class Test
-{
-     public static void main(String[] args){
-          int a=2;
-           System.out.println("Hello World"+a);    
-     }
-}
--- java_note/HackerRank/Java_Stdin_and_Stdout_II.java
+++
@@ -1,7 +0,0 @@
-package HackerRank;
-import java.util.*;
-public class Java_Stdin_and_Stdout_II {
-     public static void main(String[] args) {
-          
-     }
-}
