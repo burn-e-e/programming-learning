255817aa3ea29c189404ccce5b307fd847b822c7
Burn <76781080+burn-e-e@users.noreply.github.com>
2023-03-27 11:32:16 +0800

    push

---
+++ c#_note/C# Console App/Exception/Exception/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Class1
+{
+    static void Main(string[] args)
+    {
+        int huvaagdagch = 0, huvaagch = 0, hariu = 0;
+        try
+        {
+            hariu = huvaagdagch / huvaagch;
+        }
+        catch (System.DivideByZeroException zail)
+        {
+            Console.WriteLine("{0} exception ajillav", zail);
+        }
+        Console.ReadLine();
+    }
+}
