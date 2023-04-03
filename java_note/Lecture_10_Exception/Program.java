308c64e54e3e2ea8cfda8b15bc489dfc42f13613
Burn <76781080+burn-e-e@users.noreply.github.com>
2023-04-03 18:51:44 +0800

    exception

---
+++ java_note/Lecture_10_Exception/Program.java
@@ -0,0 +1,13 @@
+package Lecture_10_Exception;
+
+public class Program {
+     public static void main(String[] args) {
+          try {
+
+          } catch (Exception e) {
+               
+          } catch(Throwable e1){
+               
+          }
+     }
+}
