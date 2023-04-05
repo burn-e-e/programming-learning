f9a20f16ea1b08aed534d548bc2bdac6f8943b29
Burn <76781080+burn-e-e@users.noreply.github.com>
2023-04-05 11:28:01 +0800

    push

--- java_note/bd3/FirstOne/Book.java
+++ java_note/bd3/FirstOne/Book.java
@@ -1,5 +1,18 @@
 package bd3.FirstOne;
 
+import java.sql.Date;
+
 public class Book {
-     
+     String bookname;
+     String bookauthor;
+     int bookprice;
+     Date bookdate;
+     int booksumprice;
+     int bookavgprice;
+     Book(String bookname, String bookauthor, int bookprice,Date bookdate){
+           this.bookname = bookname;
+           this.bookauthor = bookauthor;
+           this.bookprice = bookprice;
+           this.bookdate = bookdate;
+     }
 }
