c2765383145c9ac7a1c1818d488a55bd89dc10d3
Burn <76781080+burn-e-e@users.noreply.github.com>
2023-04-07 12:07:42 +0800

    push

--- java_note/bd3/FirstOne/Book.java
+++ java_note/bd3/FirstOne/Book.java
@@ -1,18 +1,50 @@
 package bd3.FirstOne;
 
-import java.sql.Date;
-
 public class Book {
-     String bookname;
-     String bookauthor;
-     int bookprice;
-     Date bookdate;
-     int booksumprice;
-     int bookavgprice;
-     Book(String bookname, String bookauthor, int bookprice,Date bookdate){
-           this.bookname = bookname;
-           this.bookauthor = bookauthor;
-           this.bookprice = bookprice;
-           this.bookdate = bookdate;
-     }
+      private String bookName;
+      private String bookAuthor;
+      protected int bookPrice;
+      private String datePrinted;
+      protected static int sumOfAllBooks;
+      protected static int averagePriceOfBooks;
+
+      public Book() {
+            this.bookName = "";
+            this.bookAuthor = "";
+            this.bookPrice = 0;
+            this.datePrinted = "";
+      }
+
+      public Book(String bookName, String bookAuthor, int bookPrice, String datePrinted) {
+            this.bookName = bookName;
+            this.bookAuthor = bookAuthor;
+            this.bookPrice = bookPrice;
+            this.datePrinted = datePrinted;
+      }
+
+      public Book(Book book) {
+            this.bookName = book.bookName;
+            this.bookAuthor = book.bookAuthor;
+            this.bookPrice = book.bookPrice;
+            this.datePrinted = book.datePrinted;
+      }
+
+      public void display() {
+            System.out.println("Book name: " + bookName);
+            System.out.println("Book author: " + bookAuthor);
+            System.out.println("Book price: " + bookPrice);
+            System.out.println("Date printed: " + datePrinted);
+      }
+
+      public static void changePrice(Book book) {
+            book.bookPrice = book.bookPrice + 10;
+      }
+
+      public static int getSumOfAllBooks() {
+            return sumOfAllBooks;
+      }
+
+      public static int getAveragePriceOfBooks() {
+            return averagePriceOfBooks;
+      }
 }
--- java_note/bd3/FirstOne/BookDemo.java
+++ java_note/bd3/FirstOne/BookDemo.java
@@ -1,5 +1,32 @@
 package bd3.FirstOne;
 
-public class BookDemo {
-     
+public class BookDemo extends Book{
+     public static void main(String[] args) {
+          Book book1 = new Book("The Lord of the Rings", "J.R.R. Tolkien", 1954, "September 21");
+          Book book2 = new Book("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", 1979, "October 12");
+          Book book3 = new Book("The Da Vinci Code", "Dan Brown", 2003, "May 18");
+          Book book4 = new Book("The Hunger Games", "Suzanne Collins", 2008, "September 14");
+          Book book5 = new Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 1997, "June 26");
+  
+          sumOfAllBooks = book1.bookPrice + book2.bookPrice + book3.bookPrice + book4.bookPrice + book5.bookPrice;
+          averagePriceOfBooks = sumOfAllBooks / 5;
+  
+          book1.display();
+          book2.display();
+          book3.display();
+          book4.display();
+          book5.display();
+  
+          BookDemo.changePrice(book1);
+          BookDemo.changePrice(book3);
+  
+          book1.display();
+          book2.display();
+          book3.display();
+          book4.display();
+          book5.display();
+  
+          System.out.println("The sum of all books is " + sumOfAllBooks);
+          System.out.println("The average price of books is " + averagePriceOfBooks);
+      }
 }
