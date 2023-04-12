644b1ebad926aeda6c9337d9ba0db772049b27ce
Burn <76781080+burn-e-e@users.noreply.github.com>
2023-04-12 09:58:51 +0800

    Name change

--- java_note/Lecture_11/Give_value_to_Array.java
+++ java_note/Lecture_11/Give_value_to_Array.java
@@ -1,7 +1,7 @@
 package Lecture_11;
 import java.util.Scanner;
 
-public class Program {
+public class Give_value_to_Array {
      public static void main(String[] args) {
           Scanner sc = new Scanner(System.in);
           int size;
