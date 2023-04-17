bfb1428f6ae5f313bebf0b71fe2ea7d648215df4
Burn <76781080+burn-e-e@users.noreply.github.com>
2023-04-17 14:32:32 +0800

    Push

--- java_note/bd4/src2/Main.java
+++ java_note/bd4/src2/Main.java
@@ -1,3 +1,4 @@
+package bd4.src2;
 public class Main {
     public static void main(String[] args) {
         Person[] persons = new Person[4];
---
+++ java_note/bd4/src2/Sportsman.java
@@ -0,0 +1,25 @@
+package bd4.src2;
+
+public class Sportsman extends Person {
+    private double weight;
+    private int height;
+    String nameS;
+
+    Sportsman(String name, int age, double weight, int height) {
+        super(name, age);
+        this.weight = weight;
+        this.height = height;
+    }
+
+    @Override
+    void display() {
+        System.out.println("Sport name" + nameS + "Height: " + height + " Weight: " + weight);
+    }
+    void changeField(double weight){
+        this.weight = weight;
+    }
+    void display() {
+        System.out.println("Weight: "+weight);
+        System.out.println("Age: "+ );
+    }
+}
