f5abe5cca41dd1ae49fba3ec1a58d777f653ebdd
Burn <76781080+burn-e-e@users.noreply.github.com>
2023-04-25 10:03:43 +0800

    c#

---
+++ c#_note/C# Labs/EmployeeManagement/EmployeeManagement/Classes/contact.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Common;
+using Microsoft.Data.SqlClient;
+
+namespace EmployeeManagement.Classes
+{
+    internal class contact
+    {
+        public int contactID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ContactNo { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+
+        static string myconnectionstring = ConfigurationManager.ConnectionStrings["connectionstring"].ConnectionString;
+        //дата баазаас утга авах
+        public DataTable Select()
+        {
+            //1. Дата бааз холболт
+            SqlConnection conn = new SqlConnection(myconnectionstring);
+            DataTable dt = new DataTable();
+            try
+            {
+                //2. SQL query 
+                string sql = "SELECT * FROM tbl_management";
+                //sql болон conn ашиглан cmd үүсгэж байна
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                //cmd ашиглан SQLтохируулагч үүсгэж байна 
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                conn.Open();
+                adapter.Fill(dt);
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return dt;
+        }
+        //Дата баазлуу дата оруулах
+        public bool Insert(contact c)
+        {
+            //default буцаах утга үүсэж утгын false болгож байна
+            bool isSuccess = false;
+            //1. Дата бааз холбох
+            SqlConnection conn = new SqlConnection(myconnectionstring);
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                conn.Close ();
+            }
+            return isSuccess;
+        }
+    }
+}
---
+++ c#_note/C# Labs/EmployeeManagement/EmployeeManagement/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace EmployeeManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            title = new Label();
+            label1 = new Label();
+            txtboxContactID = new TextBox();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label5 = new Label();
+            textBox4 = new TextBox();
+            cmbGender = new ComboBox();
+            label6 = new Label();
+            btnAdd = new Button();
+            btnUpdate = new Button();
+            btnDelete = new Button();
+            btnClear = new Button();
+            dataGridView1 = new DataGridView();
+            label7 = new Label();
+            txtboxSearch = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // title
+            // 
+            title.AutoSize = true;
+            title.Font = new Font("Arial", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            title.Location = new Point(303, 39);
+            title.Name = "title";
+            title.Size = new Size(177, 32);
+            title.TabIndex = 0;
+            title.Text = "Management";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(57, 111);
+            label1.Name = "label1";
+            label1.Size = new Size(104, 28);
+            label1.TabIndex = 1;
+            label1.Text = "Contact ID";
+            // 
+            // txtboxContactID
+            // 
+            txtboxContactID.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txtboxContactID.Location = new Point(167, 108);
+            txtboxContactID.Name = "txtboxContactID";
+            txtboxContactID.Size = new Size(204, 34);
+            txtboxContactID.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(57, 162);
+            label2.Name = "label2";
+            label2.Size = new Size(106, 28);
+            label2.TabIndex = 1;
+            label2.Text = "First Name";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(167, 159);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(204, 34);
+            textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(57, 212);
+            label3.Name = "label3";
+            label3.Size = new Size(103, 28);
+            label3.TabIndex = 1;
+            label3.Text = "Last Name";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(57, 263);
+            label4.Name = "label4";
+            label4.Size = new Size(112, 28);
+            label4.TabIndex = 1;
+            label4.Text = "Contact No";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(167, 209);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(204, 34);
+            textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox3.Location = new Point(167, 260);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(204, 34);
+            textBox3.TabIndex = 2;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(57, 313);
+            label5.Name = "label5";
+            label5.Size = new Size(82, 28);
+            label5.TabIndex = 1;
+            label5.Text = "Address";
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox4.Location = new Point(167, 310);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(204, 90);
+            textBox4.TabIndex = 2;
+            // 
+            // cmbGender
+            // 
+            cmbGender.Font = new Font("Arial Narrow", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            cmbGender.FormattingEnabled = true;
+            cmbGender.Items.AddRange(new object[] { "Male", "Female" });
+            cmbGender.Location = new Point(167, 417);
+            cmbGender.Name = "cmbGender";
+            cmbGender.Size = new Size(204, 32);
+            cmbGender.TabIndex = 3;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(57, 413);
+            label6.Name = "label6";
+            label6.Size = new Size(76, 28);
+            label6.TabIndex = 1;
+            label6.Text = "Gender";
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(86, 496);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(132, 44);
+            btnAdd.TabIndex = 4;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(247, 496);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(132, 44);
+            btnUpdate.TabIndex = 4;
+            btnUpdate.Text = "Update";
+            btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(412, 496);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(132, 44);
+            btnDelete.TabIndex = 4;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(573, 496);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(132, 44);
+            btnClear.TabIndex = 4;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(391, 159);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(360, 270);
+            dataGridView1.TabIndex = 5;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(391, 108);
+            label7.Name = "label7";
+            label7.Size = new Size(70, 28);
+            label7.TabIndex = 1;
+            label7.Text = "Search";
+            // 
+            // txtboxSearch
+            // 
+            txtboxSearch.Location = new Point(467, 112);
+            txtboxSearch.Name = "txtboxSearch";
+            txtboxSearch.Size = new Size(284, 27);
+            txtboxSearch.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 583);
+            Controls.Add(txtboxSearch);
+            Controls.Add(dataGridView1);
+            Controls.Add(btnClear);
+            Controls.Add(btnUpdate);
+            Controls.Add(btnDelete);
+            Controls.Add(btnAdd);
+            Controls.Add(cmbGender);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox1);
+            Controls.Add(textBox2);
+            Controls.Add(txtboxContactID);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label2);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(title);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "EManagement";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label title;
+        private Label label1;
+        private TextBox txtboxContactID;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label5;
+        private TextBox textBox4;
+        private ComboBox cmbGender;
+        private Label label6;
+        private Button btnAdd;
+        private Button btnUpdate;
+        private Button btnDelete;
+        private Button btnClear;
+        private DataGridView dataGridView1;
+        private Label label7;
+        private TextBox txtboxSearch;
+    }
+}
