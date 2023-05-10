5bf640e76f3a344e251cff3efb594ae9cb0cf552
Burn <76781080+burn-e-e@users.noreply.github.com>
2023-05-10 10:19:08 +0800

    Push

---
+++ java_note/Java GUI/GUI.java
@@ -0,0 +1,11 @@
+package Java GUI;
+import javax.swing.*;
+public class GUI {
+     public static void main(String[] args) {
+          int size = 420;
+          JFrame frame = new JFrame();//create a frame
+          frame.setSize(size, size);//sets the x-dimension, and y-dimension of frame
+          
+          frame.setVisible(true); //make frame visible
+     }
+}
