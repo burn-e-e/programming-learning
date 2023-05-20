63287a80de5de3ba59460cdb8aa46e433f45a032
Burn <76781080+burn-e-e@users.noreply.github.com>
2023-05-20 18:47:05 +0800

    Update GUI.java

--- java_note/Java GUI/GUI.java
+++ java_note/Java GUI/GUI.java
@@ -4,7 +4,7 @@ public class GUI {
      public static void main(String[] args) {
           int size = 420;
           JFrame frame = new JFrame();//create a frame
-          frame.setSize(size, size);//sets the x-dimension, and y-dimension of frame
+          frame.setSize(size, size);
           
           frame.setVisible(true); 
      }
